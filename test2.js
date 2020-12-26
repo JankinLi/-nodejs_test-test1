5d16cde94082dd37fa14de41b499af78ad142799
jankin.li <jankin.li@outlook.com>
2020-12-26 11:20:49 +0800

    install cowsay

---
+++ test2.js
@@ -0,0 +1,10 @@
+const doSomething = () => console.log('test')
+const measureDoingSomething = () => {
+  console.time('doSomething()')
+  //do something, and measure the time it takes
+  doSomething()
+  console.timeEnd('doSomething()')
+}
+measureDoingSomething()
+
+//cowsay('Hello')
